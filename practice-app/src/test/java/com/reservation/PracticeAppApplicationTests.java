fc13f70237743c254dcf4f6d74200fe551b76436
Rikuto Kitamura <riku.tennis1080@icloud.com>
2025-11-30 16:12:29 +0900

    2025/11/30_ファーストコミット

---
+++ practice-app/src/test/java/com/reservation/PracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
